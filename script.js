41ce5bef7fc7513f29cd9b65516e63a6c4770f45
Lucas Rock <78535614+L-Rock-C@users.noreply.github.com>
2023-05-30 16:49:00 -0300

    Add macro funcionality

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 
 var macroName = document.getElementsByName('macro-name')[0];
-var macroImg = document.getElementsByName('macro-img-gif')[0];
-var macroAttkQtd = document.getElementsByName('macro-attk-qtd')[0];
 var macroAttribute = document.getElementsByName('macro-attribute')[0];
+var macroImg = document.getElementsByName('macro-img-gif')[0];
+var macroCriticalRange = document.getElementsByName('macro-critical-range')[0];
+var macroDamage = document.getElementsByName('macro-damage')[0];
+var macroCritical = document.getElementsByName('macro-critical')[0];
+var macroProficiency = document.getElementsByName('macro-pb')[0];
+var macroAttkQtd = document.getElementsByName('macro-attk-qtd')[0];
+var macroGlobalAttack = document.getElementsByName('macro-global-attk-mod')[0];
+var macroGlobalDamage = document.getElementsByName('macro-global-dmg-mod')[0];
+var macroEffectType = document.getElementsByName('macro-effect-type')[0];
+var macroEffectColor = document.getElementsByName('macro-effect-color')[0];
 
 
 var macroPreviewName = document.querySelector('#macro-preview-name');
 var macroPreviewImg = document.querySelector('#macro-preview-img');
 
+var macroOutput = document.getElementById('macro-output-textarea');
 // Update the preview name
 macroName.addEventListener('input', function(){
 	if(macroName.value == ''){
@@ -48,3 +57,150 @@ macroAttkQtd.addEventListener('input', function(){
 	}
 });
 
+
+// Generate de Macro
+macro = "";
+function genMacro(){
+	macroName = document.getElementsByName('macro-name')[0];
+	macroAttribute = document.getElementsByName('macro-attribute')[0];
+	macroImg = document.getElementsByName('macro-img-gif')[0];
+	macroCriticalRange = document.getElementsByName('macro-critical-range')[0];
+	macroDamage = document.getElementsByName('macro-damage')[0];
+	macroCritical = document.getElementsByName('macro-critical')[0];
+	macroProficiency = document.getElementsByName('macro-pb')[0];
+	macroAttkQtd = document.getElementsByName('macro-attk-qtd')[0];
+	macroGlobalAttack = document.getElementsByName('macro-global-attk-mod')[0];
+	macroGlobalDamage = document.getElementsByName('macro-global-dmg-mod')[0];
+	macroEffectType = document.getElementsByName('macro-effect-type')[0];
+	macroEffectColor = document.getElementsByName('macro-effect-color')[0];
+
+	macro = "&{template:default}{{name=" + macroName.value + "}}";
+	var attribute = "";
+	var proficiency = "";
+	var globalDamage = "";
+	var globalAttack = ""
+	var effectType = "";
+	var effectColor = "";
+	var effect = "";
+
+	if(macroImg.value != ""){
+		macro += "{{[Image](" + macroImg.value + ")}}";
+	}
+
+	if(macroAttribute.selectedIndex == 1){
+		attribute = "+@{strength_mod}";
+	} else if(macroAttribute.selectedIndex == 2){
+		attribute = "+@{dexterity_mod}";
+	} else if(macroAttribute.selectedIndex == 3){
+		attribute = "+@{constitution_mod}";
+	} else if(macroAttribute.selectedIndex == 4){
+		attribute = "+@{intelligence_mod}";
+	} else if(macroAttribute.selectedIndex == 5){
+		attribute = "+@{wisdom_mod}";
+	} else if(macroAttribute.selectedIndex == 6){
+		attribute = "+@{charisma_mod}";
+	}
+
+	if(macroProficiency.checked == true){
+		proficiency = "+@{pb}";
+	}
+
+	if(macroGlobalAttack.checked == true){
+		globalAttack = '+@{global_attack_mod}';
+	}
+
+	if(macroGlobalDamage.checked == true){
+		globalDamage = '+@{global_damage_mod_roll}';
+	}
+
+	if(macroAttkQtd.value == 1){
+		macro += "{{ Attack: [[1d20cs>" + macroCriticalRange.value + attribute + proficiency + globalAttack + "]] Advantage: [[1d20cs>" + macroCriticalRange.value +  attribute + proficiency + globalAttack + "]]}}{{ Damage: [[" + macroDamage.value + attribute + globalDamage  + "]] Critical: [[" + macroCritical.value + attribute + globalDamage  + "]]}}"; 
+	} else if(macroAttkQtd.value > 1){
+		for(let i = 1; i <= macroAttkQtd.value; i++){
+			macro += "{{" + i + "° Attack: [[1d20cs>" + macroCriticalRange.value +  attribute + proficiency + globalAttack + "]] Advantage: [[1d20cs>" + macroCriticalRange.value +  attribute + proficiency + globalAttack + "]]}}{{" + i + "° Damage: [[" + macroDamage.value + attribute + globalDamage +"]] Critical: [[" + macroCritical.value + attribute + globalDamage + "]]}}"; 
+		}
+	}
+
+	macroOutput.innerHTML = macro;
+
+	if(macroEffectType.selectedIndex != 0){
+		if(macroEffectColor.selectedIndex != 0){
+			switch(macroEffectType.selectedIndex){
+			case 1:
+				effectType = "\n/fx beam-";
+				break;
+			case 2:
+				effectType = "\n/fx bomb-";
+				break;
+			case 3:
+				effectType = "\n/fx breath-";
+				break;
+			case 4:
+				effectType = "\n/fx bubbling-";
+				break;
+			case 5:
+				effectType = "\n/fx burn-";
+				break;
+			case 6:
+				effectType = "\n/fx explode-";
+				break;
+			case 7:
+				effectType = "\n/fx glow-";
+			case 8:
+				effectType = "\n/fx missile-";
+				break;
+			case 9:
+				effectType = "\n/fx nova-";
+			case 10:
+				effectType = "\n/fx splatter-";
+				break;
+			}
+
+			switch(macroEffectColor.selectedIndex){
+			case 1:
+				effectColor = "acid";
+				break;
+			case 2:
+				effectColor = "blodd";
+				break;
+			case 3:
+				effectColor = "charm";
+				break;
+			case 4:
+				effectColor = "death";
+				break;
+			case 5:
+				effectColor = "fire";
+				break;
+			case 6:
+				effectColor = "frost";
+				break;
+			case 7:
+				effectColor = "holy";
+				break;
+			case 8:
+				effectColor = "magic";
+				break;
+			case 9:
+				effectColor = "slime";
+				break;
+			case 10:
+				effectColor = "smoke";
+				break;
+			case 11:
+				effectColor = "water";
+				break;
+			}
+			effect = effectType + effectColor;
+			macro += effect + " @{target|Source|token_id} @{target|Target|token_id}";
+			macroOutput.innerHTML = macro;
+		} else{
+			macroOutput.innerHTML = "Select an effect color!";
+		}
+	}
+}
+
+function copyMacro(){
+	navigator.clipboard.writeText(macro);
+	alert("Macro copied to clipboard!");
+}
